944136419a5acb7b5c965952fb9243a83bbb82d3
최근원 <kidjustinchoi@gmail.com>
2023-07-19 09:22:36 +0900

    chore: add eslint & prettier

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, 'src');
+const config = getDefaultConfig(__dirname, 'src')
 
-module.exports = config;
+module.exports = config
