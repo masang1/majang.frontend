5a9caefbcf551608a1d6a1348eb7ecb5f5a63427
최근원 <kidjustinchoi@gmail.com>
2023-07-19 09:16:47 +0900

    chore: set react native static path

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { SplashPng } from './assets';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={SplashPng} />
       <StatusBar style="auto" />
     </View>
   );
---
+++ assets/index.ts
@@ -0,0 +1,1 @@
+export { default as SplashPng } from "./splash.png";
