fafbbbd47389342c52f520b2c8141082a3bdf97c
choi138 <kidjustinchoi@gmail.com>
2023-07-28 16:11:58 +0900

    feat: add use fetch user

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const API_SUFFIX = {
   BASEURL: process.env.EXPO_PUBLIC_BASE_URL,
   AUTH: '/users',
+  GET_USER: '/users/@me',
 };
 
 export const instance = axios.create({
--- src/components/home/Header/styled.ts
+++ src/components/home/Header/styled.ts
@@ -7,9 +7,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { colors } from 'src/styles';
 
 export const HeaderContainer = styled.View`
-  position: absolute;
-  top: 0;
-  padding: ${Platform.OS === 'android' ? '40px' : '60px'} 22px 20px 22px;
+  padding: ${Platform.OS === 'android' ? '40px' : '60px'} 22px 40px 22px;
   width: 100%;
   flex-direction: row;
   align-items: center;
