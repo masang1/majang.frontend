77f5c22a79e50f4b966bfe9f06aed6c41a1fab83
choi138 <kidjustinchoi@gmail.com>
2023-07-28 14:21:35 +0900

    chore: change component name main -> home

---
+++ src/components/home/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import * as S from './styled';
+export const Footer: React.FC = () => {
+  return (
+    <S.FooterContainer>
+      <S.FooterText> Footer </S.FooterText>
+    </S.FooterContainer>
+  );
+};
