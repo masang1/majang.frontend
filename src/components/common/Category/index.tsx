733e606461eb0b6b92fc04707b2262132404a229
최근원 <kidjustinchoi@gmail.com>
2023-07-30 17:08:03 +0900

    feat: add category component

---
+++ src/components/common/Category/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styled';
+
+export const Category: React.FC = () => {
+  return (
+    <S.CategoryContainer>
+      <S.CategoryTitle>Category</S.CategoryTitle>
+    </S.CategoryContainer>
+  );
+};
---
+++ src/components/common/Category/styled.ts
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components/native';
+
+export const CategoryContainer = styled.View`
+  padding: 0 22px;
+`;
+
+export const CategoryTitle = styled.Text``;
