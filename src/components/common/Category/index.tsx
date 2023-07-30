8acdd83a63a16f9b08b4ecf25e9282d38d9b6842
최근원 <kidjustinchoi@gmail.com>
2023-07-30 17:30:29 +0900

    feat: add category icons in component

--- src/components/common/Category/index.tsx
+++ src/components/common/Category/index.tsx
@@ -1,9 +1,24 @@
+import { WithLocalSvg } from 'react-native-svg';
+
+import { CATEGORY_LIST } from 'src/constant';
+
+import { Text } from '../Text';
+
 import * as S from './styled';
 
 export const Category: React.FC = () => {
   return (
     <S.CategoryContainer>
-      <S.CategoryTitle>Category</S.CategoryTitle>
+      {CATEGORY_LIST.map(({ text, icon }, i) => (
+        <S.CategoryItemContainer key={i}>
+          <S.CategoryItemWrapper activeOpacity={0.5}>
+            <WithLocalSvg asset={icon} width={36} height={36} />
+          </S.CategoryItemWrapper>
+          <Text size={14} weight={700}>
+            {text}
+          </Text>
+        </S.CategoryItemContainer>
+      ))}
     </S.CategoryContainer>
   );
 };
--- src/components/common/Category/styled.ts
+++ src/components/common/Category/styled.ts
@@ -2,6 +2,25 @@ import { styled } from 'styled-components/native';
 
 export const CategoryContainer = styled.View`
   padding: 0 22px;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  row-gap: 10px;
 `;
 
-export const CategoryTitle = styled.Text``;
+export const CategoryItemContainer = styled.View`
+  flex-direction: column;
+  row-gap: 6px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CategoryItemWrapper = styled.TouchableOpacity`
+  width: 55px;
+  height: 55px;
+  border-radius: 12px;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8f8f8;
+`;
